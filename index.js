b108c85474becd66fed69cac01338352ab17de85
Craig Spence <craigspence0@gmail.com>
2020-06-24 12:41:32 +1200

    fix(🐛): handle a few edge cases

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const DEFAULT_EMOJI_MAP = {
   fix: "🐛",
   perf: "🏎",
   refactor: "🔧",
+  release: "🔖",
   revert: "💥",
   style: "😎",
   test: "🚨",
 };
+
+const PARSE_EMOJI = /(\p{Emoji_Presentation})$/u;
+const PARSE_SCOPE = /^(\w*) (\p{Emoji_Presentation})$/u;
 
 module.exports = {
   rules: {
@@ -20,16 +24,27 @@ module.exports = {
       value = DEFAULT_EMOJI_MAP
     ) => {
       if (!scope) {
-        return [false, `Commit scope must always be set`];
+        return [false, `commit scope should be set`];
       }
-      const expectedEmoji = value[type];
-      if (!expectedEmoji) {
-        return [false, `Scope emoji not specified for commit type "${type}"`];
+      const expectedTypeEmoji = value[type];
+      const [, scopeEmoji] = scope.match(PARSE_EMOJI) || [];
+      const [, scopeName, emoji] = scope.match(PARSE_SCOPE) || [];
+      if (!scopeEmoji || !scopeName || !emoji) {
+        return [
+          false,
+          `commit message with type "${type}" should have scope = (${expectedTypeEmoji}) or scope = (myscope ${expectedTypeEmoji})`,
+        ];
       }
-      const validScope = scope.endsWith(expectedEmoji);
+      const expectedScopeEmoji = value[scopeName];
+      if (expectedScopeEmoji) {
+        return [
+          expectedScopeEmoji === emoji,
+          `commit message with type "${type}" and scope "${scopeName}" should begin ${type}(${scopeName} ${expectedScopeEmoji})`,
+        ];
+      }
       return [
-        validScope,
-        `Commits with type "${type}" must have a scope ending with "${expectedEmoji}"`,
+        expectedTypeEmoji === emoji,
+        `commit message with type "${type}" and scope "${scopeName}" should begin ${type}(${scopeName} ${expectedTypeEmoji})`,
       ];
     },
   },
---
+++ index.test.js
@@ -0,0 +1,62 @@
+const assert = require("assert");
+const { rules } = require("./index");
+
+[
+  {
+    input: {},
+    expected: { result: false, message: `commit scope should be set` },
+  },
+  {
+    input: { type: "build", scope: "compiler" },
+    expected: {
+      result: false,
+      message: `commit message with type "build" should have scope = (🔨) or scope = (myscope 🔨)`,
+    },
+  },
+  {
+    input: { type: "docs", scope: "compiler 🎉" },
+    expected: {
+      result: false,
+      message: `commit message with type "docs" and scope "compiler" should begin docs(compiler 📚)`,
+    },
+  },
+  {
+    input: { type: "docs", scope: "compiler  📚" },
+    expected: {
+      result: false,
+      message: `commit message with type "docs" should have scope = (📚) or scope = (myscope 📚)`,
+    },
+  },
+  {
+    input: { type: "docs", scope: "two words 📚" },
+    expected: {
+      result: false,
+      message: `commit message with type "docs" should have scope = (📚) or scope = (myscope 📚)`,
+    },
+  },
+  {
+    input: { type: "docs", scope: "compiler 📚" },
+    expected: {
+      result: true,
+    },
+  },
+  {
+    input: { type: "chore", scope: "release 📚" },
+    expected: {
+      result: false,
+      message: `commit message with type "chore" and scope "release" should begin chore(release 🔖)`,
+    },
+  },
+  {
+    input: { type: "chore", scope: "release 🔖" },
+    expected: {
+      result: true,
+    },
+  },
+].forEach(({ input, expected }) => {
+  const [result, message] = rules["@phenomnomnominal/scopes"](input);
+  assert.equal(result, expected.result);
+  if (!result) {
+    assert.equal(message, expected.message);
+  }
+});
